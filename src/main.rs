c68e3657b1a3547322228b13c545748c1caf8c6f
bishtpawan <pawan.bisht@knoldus.com>
2021-06-09 19:41:12 +0530

    Initail commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#![no_main]
+#![no_std]
+
+pub use panic_itm;
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    loop {}
+}
+
+
+
+/*
+//pub use panic_itm;
+use cortex_m_rt::entry;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_panic_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+//#[entry]
+fn main() -> ! {
+    let _company_name = "KNOLDUS";
+    let _practice_area = "RUST_EMBEDDED";
+    let _device_name = "STM32F3DISCOVERY";
+
+    //
+    loop {}
+}
+*/
