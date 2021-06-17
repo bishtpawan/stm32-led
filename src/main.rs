9a52a78a2a92e307df4d57dbff15718d282d43c7
bishtpawan <pawan.bisht@knoldus.com>
2021-06-17 13:28:38 +0530

    Add basic code for cross compilation

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,20 @@
 #![no_main]
 #![no_std]
 
+
 pub use panic_itm;
 use cortex_m_rt::entry;
 
-#[entry]
-fn main() -> ! {
-    loop {}
-}
-
-
-
 /*
-//pub use panic_itm;
-use cortex_m_rt::entry;
-
+// Custom panic implementation
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_panic_info: &PanicInfo) -> ! {
     loop {}
-}
+}*/
 
-//#[entry]
+#[entry]
 fn main() -> ! {
     let _company_name = "KNOLDUS";
     let _practice_area = "RUST_EMBEDDED";
@@ -31,4 +23,4 @@ fn main() -> ! {
     //
     loop {}
 }
-*/
+
